6b01ecc72ee326f4405209c4e41c3820dd3ac0d6
Damustin <damianamlp@hotmail.com>
2023-08-15 20:36:08 -0300

    until first  template running ok

--- DJP/urls.py
+++ DJP/urls.py
@@ -2,7 +2,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',HomeView.as_view(), name='home')
 ]
